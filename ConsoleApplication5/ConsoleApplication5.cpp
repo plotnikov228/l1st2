9f0c6aef90163d6fbf0674e6399790ce4916754c
user <user@WIN-BDAHPTRT5BE>
2022-03-17 18:27:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <list>
+#include <algorithm>
+#include <iterator>
+
+bool isOdd(int num) {
+    return (num & 1);
+}
+
+int main() {
+    std::list<int> L;
+    L.push_back(5);
+    L.push_back(2);
+    L.push_back(6);
+    L.push_back(9);
+    L.push_back(4);
+    L.push_back(3);
+    L.push_back(8);
+
+    std::cout << "All list:" << std::endl;
+    std::copy(L.begin(), L.end(), std::ostream_iterator<int>(std::cout, " "));
+
+    L.remove_if(isOdd);
+
+    std::cout << std::endl << "Without odds:" << std::endl;
+    std::copy(L.begin(), L.end(), std::ostream_iterator<int>(std::cout, " "));
+
+    std::cout << std::endl;
+
+    return 0;
+}
