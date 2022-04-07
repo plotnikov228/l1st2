8c3fc94361fd4198aec6fa3f505b9cf0bdee4d76
plotnikov228 <92577632+plotnikov228@users.noreply.github.com>
2022-04-07 13:45:36 +0300

    Update ConsoleApplication5.cpp

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -1,35 +1,136 @@
 #include <iostream>
-#include <list>
-#include <algorithm>
-#include <iterator>
+#include <cstdlib>
 
 using namespace std;
 
+class Node
+{
+public:
+    Node* next;
+    int data;
+    Node();
+    ~Node();
+    void print();
+    
+};
 
-bool isOdd(int num) {
-    if (num % 2 == 0)
-    return 1;
+class LinkedList
+{
+public:
+    int length;
+    Node* head;
+
+    LinkedList();
+    ~LinkedList();
+    void add(int data);
+    void remove(int data);
+    void toArray(int arr[], int len);
+    void add_back(int data);
+    void print();
+
+};
+
+Node::Node() {
+    //set default values;
 }
 
-int main() {
-    list<int> L;
-    L.push_back(5);
-    L.push_back(2);
-    L.push_back(6);
-    L.push_back(9);
-    L.push_back(4);
-    L.push_back(3);
-    L.push_back(8);
+Node::~Node() {
+    cout << "NODE DELETED" << endl;
+}
 
-    cout << "All list:" << endl;
-    copy(L.begin(), L.end(), ostream_iterator<int>(cout, " "));
+void Node::print() {
+    Node* node = this;
+    if (node != NULL) {
+        cout << "===============================" << endl;
+        cout << this->data << endl;
+        cout << "===============================" << endl;
+    }
+}
 
-    L.remove_if(isOdd);
+LinkedList::LinkedList() {
+    this->length = 0;
+    this->head = NULL;
+}
 
-    cout << endl << "Without odds:" << endl;
-    copy(L.begin(), L.end(), ostream_iterator<int>(cout, " "));
+LinkedList::~LinkedList() {
+    cout << "LIST DELETED" << endl;
+}
 
-    cout << endl;
+void LinkedList::add(int data) {
+    Node* node = new Node();
+    node->data = data;
+    node->next = this->head;
+    this->head = node;
+    this->length++;
+}
+
+
+void LinkedList::print() {
+    if (this->length == 0) {
+        cout << "The list is empty" << endl;
+    }
+    else {
+
+        Node* head = this->head;
+        int i = 0;
+        cout << "List:" << endl;
+        cout << "===============================" << endl;
+        while (head) {
+            cout << i << ": " << head->data << endl;
+            head = head->next;
+            i++;
+        }
+        cout << "===============================" << endl;
+    }
+}
+
+
+void LinkedList::toArray(int arr[], int len) {
+        Node* head = this->head;
+        arr[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = head->data;
+            head = head->next;
+        }         
+}
+
+
+int main(int argc, char const* argv[])
+{
+    LinkedList* list = new LinkedList();
+    const int length = 7;
+
+    list->add(2);
+    list->add(4);
+    list->add(5);
+    list->add(7);
+    list->add(10);
+    list->add(13);
+    list->add(15);
+
+
+    int Array[length];
+
+
+    
+
+    list->print();
+
+
+    list->toArray(Array, length);
+
+    int n = sizeof(Array) / sizeof(int);
+    int j = 0;
+    for (int i = 0; i < n; i++)
+        if ((Array[i] % 2) == 0)
+            Array[j++] = Array[i];
+    n = j;
+    cout << "List after filter:" << endl;
+    cout << "===============================" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << i << ": " << Array[i] << endl;
+    }
+    cout << "===============================" << endl;
 
     return 0;
 }
